7dee54b3ebd2f245e750162bf97c6cd886e592cc
sang tran <tony_633@hotmail.com>
2016-10-25 14:38:39 -0400

    first commit

---
+++ api.py
@@ -0,0 +1,301 @@
+"""Hangman API implemented using Google Cloud Endpoints.
+
+api.py. Contains declarations of endpoint, endpoint methods,
+as well as the ProtoRPC message class and container required
+for endpoint method definition.
+"""
+
+import endpoints
+# PROTORPC is a library import modules
+from protorpc import messages
+from protorpc import message_types
+from protorpc import remote
+
+from google.appengine.ext import ndb
+
+from models import User, Game, Score
+from models import StringMessage, GameForm
+from models import UserForm, UserForms, HistoryForm
+from models import GameForms, CancelForm, ScoreForm, ScoreForms
+from utils import get_by_urlsafe
+
+import logging
+import datetime
+
+
+# ENDPOINT REQUEST
+# ---------------------------
+USER_REQUEST = endpoints.ResourceContainer(
+    name=messages.StringField(1, required=True),
+    email=messages.StringField(2, required=True))
+
+NEW_GAME_REQUEST = endpoints.ResourceContainer(
+    name=messages.StringField(1, required=True),
+    topic=messages.StringField(2, required=True),
+    answer=messages.StringField(3, required=True),
+    attempts_remaining=messages.IntegerField(4))
+
+CURRENT_GAME_REQUEST = endpoints.ResourceContainer(
+    urlsafe_game_key=messages.StringField(1))
+
+MAKE_MOVE_REQ = endpoints.ResourceContainer(
+    urlsafe_game_key=messages.StringField(1, required=True),
+    guess=messages.StringField(2, required=True))
+
+
+CANCEL_GAME_REQUEST = endpoints.ResourceContainer(
+    user_name=messages.StringField(1, required=True),
+    urlsafe_game_key=messages.StringField(2, required=True))
+
+HIGHEST_SCORES_REQUEST = endpoints.ResourceContainer(
+    message_types.VoidMessage,
+    number_of_results=messages.IntegerField(1),
+)
+
+# ----------------------------
+
+
+@endpoints.api(name='hangman_api', version='v1')
+class HangmanApi(remote.Service):
+
+    # CREATE USER endpoint
+    @endpoints.method(USER_REQUEST,
+                      StringMessage,
+                      path='game/create_user',
+                      name='create_user',
+                      http_method='POST')
+    def create_user(self, request):
+        """Create a User. Requires a unique username"""
+        if User.query(User.name == request.name).get():
+            raise endpoints.ConflictException(
+                'A User with that name already exists!')
+        user = User(name=request.name, email=request.email)
+        user.put()
+        return StringMessage(message='User {} created!'.format(request.name))
+
+    # NEW GAME endpoint
+    # IF ANSWER HAS A SPACE IN IT REPLACE WITH COMMA
+    @endpoints.method(NEW_GAME_REQUEST,
+                      GameForm,
+                      path='game/new_game',
+                      name='new_game',
+                      http_method='POST')
+    def new_game(self, request):
+        """Creates new game"""
+        user = User.query(User.name == request.name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                'A User with that name does not exist!')
+        try:
+            # create hidden blanks array
+            hidden = []
+            # applylowercase to answer
+            request.answer.lower()
+            # fill hidden array with blanks
+            for char in request.answer:
+                if char == " ":
+                    hidden.append(",")
+                else:
+                    hidden.append("_")
+
+            guesses = []
+            date = datetime.datetime.now()
+
+            # check if request.attempts_remaining is empty
+            if not request.attempts_remaining:
+                request.attempts_remaining = 6
+            game = Game.new_game(user.key,
+                                 request.topic,
+                                 request.answer,
+                                 hidden,
+                                 guesses,
+                                 date,
+                                 request.attempts_remaining)
+
+            # store active game urlsafe into user for reference later
+            user.activeGameKeys.append(game.key.urlsafe())
+            user.put()
+
+        except ValueError:
+            raise endpoints.BadRequestException('Cant create game')
+
+        msg = 'Good luck playing Hangman!'
+        game.add_to_history(msg, 'NONE')
+        return game.to_form(msg)
+
+    # GET Current Game endpoint
+    @endpoints.method(CURRENT_GAME_REQUEST,
+                      GameForm,
+                      path='game/{urlsafe_game_key}',
+                      name='get_current_game',
+                      http_method='GET')
+    def get_current_game(self, request):
+        """Returns a game state with message"""
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if game:
+            return game.to_form('Time to make a move!')
+        else:
+            raise endpoints.NotFoundException('Game not found!')
+
+    # MAKE MOVE endpoint
+    @endpoints.method(MAKE_MOVE_REQ,
+                      GameForm,
+                      path='game/{urlsafe_game_key}/make_move',
+                      name='make_move',
+                      http_method='PUT')
+    def make_move(self, request):
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        user = game.key.parent().get()
+
+        # take guess and apply lowercase
+        guess = request.guess.lower()
+        # 1.check game already over
+        if game.game_over:
+            msg = 'Game already over!'
+            game.add_to_history(msg, 'NONE')
+            return game.to_form(msg)
+
+        # 2. If guess is a digit
+        else:
+            if guess.isdigit():
+                msg = 'Please enter a letter.'
+            # 3. IF more than one character
+            elif len(guess) != 1:
+                msg = "Please enter a single letter!"
+            # 4. IF they already guessed same character
+            elif guess in game.guesses:
+                msg = "You already guessed that! You have {} tries".format(
+                    game.attempts_remaining)
+            # 5. guess not in answer
+            elif guess not in game.answer:
+                # minus 1 attempt
+                game.attempts_remaining -= 1
+                game.guesses.append(guess)
+                if game.attempts_remaining < 1:
+                    game.end_game(False)
+                    msg = 'Game over! You LOSE'
+                else:
+                    msg = "Nope Sorry! You have {} tries".format(
+                        game.attempts_remaining)
+            # 6. guess in answer
+            elif guess in game.answer:
+                game.guesses.append(guess)
+                # update hidden
+                for idx, item in enumerate(game.answer):
+                    if game.answer[idx] == guess:
+                        game.hidden[idx] = guess
+                # check to make sure all "_" are gone to return win
+                if "_" not in game.hidden:
+                    game.end_game(True)
+                    msg = "CONGRATS You WON! The secret was {}".format(
+                        game.answer)
+                    # return game.to_form(msg)
+                else:
+                    msg = "Nice! Looks like you guessed right"
+            game.add_to_history(msg, guess)
+            game.put()
+        return game.to_form(msg)
+
+    # GET USER GAMES endpoint
+    @endpoints.method(USER_REQUEST,
+                      GameForms,
+                      path='game/get_activeGames',
+                      name='get_user_games',
+                      http_method='GET')
+    def get_user_games(self, request):
+        """Returns all of an individual User's ACTIVE games"""
+        user = User.query(User.name == request.name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                'A User with that name does not exist!')
+
+        game_keys = [ndb.Key(urlsafe=wsck) for wsck in user.activeGameKeys]
+        games = ndb.get_multi(game_keys)
+        return GameForms(items=[game.to_form() for game in games])
+
+    # CANCEL GAME ENDPOINT
+    @endpoints.method(CANCEL_GAME_REQUEST,
+                      StringMessage,
+                      path='game/{urlsafe_game_key}/cancel',
+                      name='cancel_game',
+                      http_method='DELETE')
+    def cancel_game(self, request):
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                'A User with that name does not exist!')
+
+        """Returns a game state with message"""
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if not game:
+            raise endpoints.NotFoundException('Game not found!')
+
+        # check game is completed
+        if game.game_over:
+            raise endpoints.NotFoundException(
+                "Sorry. You can't delete a completed game")
+        else:
+            # delete activegame from user
+            if request.urlsafe_game_key in user.activeGameKeys:
+                user.activeGameKeys.remove(request.urlsafe_game_key)
+
+                ndb.Key(urlsafe=request.urlsafe_game_key).delete()
+
+                user.put()
+                return StringMessage(message='Game has been cancelled')
+
+    # GET HIGH SCORES endpoint
+    @endpoints.method(HIGHEST_SCORES_REQUEST,
+                      ScoreForms,
+                      path='game/get_high_scores',
+                      name='get_high_scores',
+                      http_method='GET')
+    def get_high_scores(self, request):
+        # order scores descending order by attempts remaining
+        # more attempts equal better
+        scores = Score.query().order(-Score.attempts_remaining)
+
+        if request.number_of_results:
+            return ScoreForms(
+                items=[score.to_form() for score in scores.fetch(
+                    limit=request.number_of_results)])
+        else:
+            return ScoreForms(items=[score.to_form() for score in scores])
+
+    # GET USERS endpoint
+    @endpoints.method(USER_REQUEST,
+                      UserForm,
+                      path='game/get_users',
+                      name='get_user',
+                      http_method='GET')
+    def get_user(self, request):
+        user = User.query(User.name == request.name).get()
+        return user.to_form()
+
+    # GET USER RANKINGS endpoint
+    # use performance display win/loss ratio
+    @endpoints.method(message_types.VoidMessage,
+                      UserForms,
+                      path='game/get_user_rankings',
+                      name='get_user_rankings',
+                      http_method='GET')
+    def get_user_rankings(self, request):
+        users = User.query().order(-User.performance)
+        return UserForms(items=[user.to_form() for user in users])
+
+    # GET_GAME_HISTORY endpoint
+    # GET Current Game
+    @endpoints.method(CURRENT_GAME_REQUEST,
+                      StringMessage,
+                      path='game/{urlsafe_game_key}/history',
+                      name='get_game_history',
+                      http_method='GET')
+    def get_game_history(self, request):
+        """Returns a summary of a game's guesses."""
+        game = get_by_urlsafe(request.urlsafe_game_key, Game)
+        if not game:
+            raise endpoints.NotFoundException('Game not found')
+        return StringMessage(message=str(game.history))
+
+
+APPLICATION = endpoints.api_server([HangmanApi])
---
+++ models.py
@@ -0,0 +1,194 @@
+"""models.py - This file contains the class definitions for the Datastore
+entities used by the Game. Because these classes are also regular Python
+classes they can include methods (such as 'to_form' and 'new_game')."""
+
+from __future__ import division
+import datetime
+from protorpc import messages
+from protorpc import message_types
+from google.appengine.ext import ndb
+import logging
+
+
+class User(ndb.Model):
+
+    """User profile"""
+    name = ndb.StringProperty(required=True)
+    email = ndb.StringProperty()
+    activeGameKeys = ndb.StringProperty(repeated=True)
+    wins = ndb.IntegerProperty(default=0)
+    losses = ndb.IntegerProperty(default=0)
+    performance = ndb.FloatProperty(default=0)
+
+    def to_form(self):
+        return UserForm(name=self.name, email=self.email,
+                        activeGameKeys=self.activeGameKeys,
+                        wins=self.wins, losses=self.losses,
+                        performance=self.performance)
+
+
+class UserForm(messages.Message):
+    name = messages.StringField(1, required=True)
+    email = messages.StringField(2)
+    activeGameKeys = messages.StringField(3, repeated=True)
+    wins = messages.IntegerField(4)
+    losses = messages.IntegerField(5)
+    performance = messages.FloatField(6)
+
+
+class UserForms(messages.Message):
+
+    """Return multiple ScoreForms"""
+    items = messages.MessageField(UserForm, 1, repeated=True)
+
+
+class Game(ndb.Model):
+
+    """Game object"""
+    game_over = ndb.BooleanProperty(required=True, default=False)
+    user = ndb.KeyProperty(required=True, kind='User')
+    topic = ndb.StringProperty(required=True)
+    answer = ndb.StringProperty(required=True)
+    attempts_remaining = ndb.IntegerProperty()
+    hidden = ndb.StringProperty(repeated=True)
+    guesses = ndb.StringProperty(repeated=True)
+    date_created = ndb.DateTimeProperty()
+    # list of history
+    history = ndb.PickleProperty(required=True, default=[])
+
+    @classmethod
+    def new_game(cls, user, topic, answer, hidden, guesses,
+                 date, attempts_remaining):
+        """Creates and returns a new game"""
+        game = Game(user=user,
+                    parent=user,
+                    topic=topic,
+                    answer=answer,
+                    hidden=hidden,
+                    guesses=guesses,
+                    game_over=False,
+                    date_created=date,
+                    attempts_remaining=attempts_remaining)
+        game.put()
+        return game
+
+    def to_form(self, message='test'):
+        """Returns a GameForm representation of the Game"""
+        form = GameForm()
+        form.topic = self.topic
+        form.hidden = self.hidden
+        form.guesses = self.guesses
+        form.urlsafe_key = self.key.urlsafe()
+        form.user_name = self.user.get().name
+        form.attempts_remaining = self.attempts_remaining
+        form.game_over = self.game_over
+        form.message = message
+        form.date_created = self.date_created.strftime("%Y-%m-%d %I:%M:%S %p")
+        return form
+
+    def to_form_all(self):
+        return GamesForm(user=self.user.get().name,
+                         topic=self.topic,
+                         game_over=self.game_over,
+                         attempts_remaining=self.attempts_remaining)
+
+    def end_game(self, won=False):
+        """Ends the game - if won is True, the player won. - if won is False,
+        the player lost."""
+        self.game_over = True
+
+        # get user thats playing this game aka parent
+        # remove active game urlsafekey element from array from user
+        user = self.key.parent().get()
+        user.activeGameKeys.remove(self.key.urlsafe())
+
+        # IF WON is false or true
+        if won:
+            user.wins += 1
+        else:
+            user.losses += 1
+
+        total_games = user.wins + user.losses
+        user.performance = (user.wins / total_games) * 100
+
+        self.put()
+        user.put()
+        # Add the game to the score 'board'
+        score = Score(name=self.user.get().name,
+                      game=self.key, user=self.user,
+                      date=datetime.datetime.now(),
+                      won=won,
+                      attempts_remaining=self.attempts_remaining)
+        score.put()
+
+    def add_to_history(self, result, guess):
+        # get history
+        self.history.append({'message': result, 'guess': guess})
+        self.put()
+
+
+class HistoryForm(messages.Message):
+
+    """Return history"""
+    history = messages.StringField(1, repeated=True)
+    date_created = messages.StringField(2)
+
+
+class GameForm(messages.Message):
+
+    """GameForm for outbound game state information"""
+    urlsafe_key = messages.StringField(1, required=True)
+    attempts_remaining = messages.IntegerField(2, required=True)
+    game_over = messages.BooleanField(3, required=True)
+    message = messages.StringField(4, required=True)
+    user_name = messages.StringField(5, required=True)
+    topic = messages.StringField(6, required=True)
+    hidden = messages.StringField(7, repeated=True)
+    guesses = messages.StringField(8, repeated=True)
+    date_created = messages.StringField(9)
+
+class GameForms(messages.Message):
+
+    """Return multiple ScoreForms"""
+    items = messages.MessageField(GameForm, 1, repeated=True)
+
+
+class Score(ndb.Model):
+
+    """Score object"""
+    game = ndb.KeyProperty(required=True, kind='Game')
+    user = ndb.KeyProperty(required=True, kind='User')
+    name = ndb.StringProperty(required=True)
+    date = ndb.DateTimeProperty(required=True)
+    won = ndb.BooleanProperty(required=True)
+    attempts_remaining = ndb.IntegerProperty(required=True)
+
+    def to_form(self):
+        return ScoreForm(user_name=self.user.get().name,
+                         won=self.won,
+                         date=self.date.strftime("%Y-%m-%d %I:%M:%S %p"),
+                         attempts_remaining=self.attempts_remaining)
+
+
+class ScoreForm(messages.Message):
+    user_name = messages.StringField(1, required=True)
+    date = messages.StringField(2, required=True)
+    won = messages.BooleanField(3, required=True)
+    attempts_remaining = messages. IntegerField(4, required=True)
+
+
+class ScoreForms(messages.Message):
+
+    """Return multiple ScoreForms"""
+    items = messages.MessageField(ScoreForm, 1, repeated=True)
+
+
+class CancelForm(messages.Message):
+    user_name = messages.StringField(1, required=True)
+    urlsafe_key = messages.StringField(2, required=True)
+
+
+class StringMessage(messages.Message):
+
+    """StringMessage-- outbound (single) string message"""
+    message = messages.StringField(1, required=True)
